4d0d6a58591bd350f31dd58259a83181e1235b91
heyuanpeng <991800152@qq.com>
2020-08-19 09:30:35 +0800

    添加table

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from "react";
 import "../css/App.css";
 import Test from "./test";
 import Boss from "./Boos";
-import axios from "axios"
+import Table from "./mayTable";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -15,19 +16,20 @@ class App extends Component {
       list: [],
     };
   }
-  componentWillReceiveProps(){
-
-  }
-  componentDidMount(){
+  componentWillReceiveProps() {}
+  componentDidMount() {
     // 写在mount中避免多次加载，建议将axios写在mount中
-    axios.get('https://www.easy-mock.com/mock/5eeec4c94d226b23f958e1ef/example/reactAv')
-      .then(  response=> {
+    axios
+      .get(
+        "https://www.easy-mock.com/mock/5eeec4c94d226b23f958e1ef/example/reactAv"
+      )
+      .then((response) => {
         console.log(response);
         this.setState({
-          list:response.data.data
-        })
+          list: response.data.data,
+        });
       })
-      .catch( (error)=> {
+      .catch((error) => {
         console.log(error);
       });
     /* axios.post('/user', {
@@ -42,7 +44,6 @@ class App extends Component {
       }); */
   }
   render() {
-   
     return (
       <Fragment>
         {/* 第一次写注释 */}
@@ -52,12 +53,18 @@ class App extends Component {
             <input
               id="1"
               value={this.state.inputValue}
-              ref={(input)=>{this.input=input}}
+              ref={(input) => {
+                this.input = input;
+              }}
               onChange={this.handelOnChange}
             ></input>
             <button onClick={this.handelAddList}>点赞</button>
           </p>
-          <ul ref={(ul)=>{this.ul=ul}}>
+          <ul
+            ref={(ul) => {
+              this.ul = ul;
+            }}
+          >
             {this.state.list.map((item, index) => {
               return (
                 <li key={index + item}>
@@ -66,28 +73,32 @@ class App extends Component {
                     index={index}
                     handelDelete={this.handelDelete}
                   />
-                </li> 
+                </li>
               );
             })}
           </ul>
         </div>
         <Boss></Boss>
+        <Table></Table>
       </Fragment>
     );
   }
-  handelOnChange(e) { 
+  handelOnChange(e) {
     this.setState({
-      inputValue:this.input.value // e.target.value,
+      inputValue: this.input.value, // e.target.value,
     });
   }
   handelAddList() {
     // setState属于异步函数，所以在使用的时候我们需要做统计是要在她的回调函数中统计
-    this.setState({
-      inputValue: "",
-      list: [...this.state.list, this.state.inputValue],
-    },(()=>{
-        console.log(this.ul.querySelectorAll("li").length)
-    }));
+    this.setState(
+      {
+        inputValue: "",
+        list: [...this.state.list, this.state.inputValue],
+      },
+      () => {
+        console.log(this.ul.querySelectorAll("li").length);
+      }
+    );
   }
   handelDelete(index) {
     let list = this.state.list;
--- src/js/Boos.js
+++ src/js/Boos.js
@@ -1,26 +1,28 @@
-import React, { Component,Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 class Boos extends Component {
   constructor(props) {
     super(props);
-    this.handelBoss=this.handelBoss.bind(this)
-    this.state = { 
-      isShow:false
-     }
+    this.handelBoss = this.handelBoss.bind(this);
+    this.state = {
+      isShow: false,
+    };
   }
-  render() { 
-    return ( 
+  render() {
+    return (
       <Fragment>
-        <div className={this.state.isShow?"show":"hide"}>boss名称</div>
-        <div><button onClick={this.handelBoss}>召唤boss</button></div>
+        <div className={this.state.isShow ? "show" : "hide"}>boss名称</div>
+        <div>
+          <button onClick={this.handelBoss}>召唤boss</button>
+        </div>
       </Fragment>
-     );
-  } 
-  handelBoss(){
+    );
+  }
+  handelBoss() {
     console.log(1);
     this.setState({
-      isShow:this.state.isShow?false:true
-    })
+      isShow: this.state.isShow ? false : true,
+    });
   }
 }
- 
+
 export default Boos;
